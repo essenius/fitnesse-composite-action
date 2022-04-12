b60135c8f1637ac321939c92fac45e4c97fdb81d
Rik Essenius <essenius@users.noreply.github.com>
2022-04-12 14:37:09 +0200

    Updated run.ps1 to enable nunit3 format

--- run.ps1
+++ run.ps1
@@ -1,7 +1,5 @@
 [CmdletBinding(DefaultParameterSetName = 'None')]
 param()
-gci env:
-
 Get-Module -Name "FitNesseConvert" | Remove-Module
 Import-Module -Name "$PSScriptRoot/FitNesseConvert.psm1"
 $rawResultsPath = "result.xml"
@@ -11,17 +9,7 @@ $details = (Convert-Xml -InputXml $testResult -XsltFile "FitNesseToDetailedResul
 $detailsPath = "DetailedResults.html"
 $details | Out-File -FilePath $detailsPath | Out-Null
 
-#Get-Module -Name "FitNesseConvert" | Remove-Module
-#Import-Module -Name "$PSScriptRoot/FitNesseConvert.psm1"
-#$root = Split-Path -Path $PSScriptRoot -Parent
-#$testresultsPath = Join-Path -Path $root -ChildPath "testresult"
-#$rawResultsPath = Join-Path -Path $testresultsPath -ChildPath "result.xml"
-#$content = Get-Content -Path "$rawResultsPath"
-#[xml]$testResult = [xml](Read-Xml -RawInput $content)
-#$details = (Convert-Xml -InputXml $testResult -XsltFile "FitNesseToDetailedResults.xslt")
-#$detailsPath = "$testResultsPath/DetailedResults.html"
-#$details | Out-File -FilePath $detailsPath | Out-Null
-#$nUnitOutput = Convert-Xml -InputXml $testResult -XsltFile "FitNesseToNUnit3.xslt"
-#$nUnitOutputWithEnv = [xml](Edit-Environment -NUnitXml $nUnitOutput)
-#$nUnitOutputFinal = [xml](Edit-Attachments -NUnitXml $nUnitOutputWithEnv -RawResults $RawResultsPath -Details $detailsPath)
-#$nUnitOutputFinal.Save("$testResultsPath/results_nunit.xml")
+$nUnitOutput = Convert-Xml -InputXml $testResult -XsltFile "FitNesseToNUnit3.xslt"
+$nUnitOutputWithEnv = [xml](Edit-Environment -NUnitXml $nUnitOutput)
+$nUnitOutputFinal = [xml](Edit-Attachments -NUnitXml $nUnitOutputWithEnv -RawResults $RawResultsPath -Details $detailsPath)
+$nUnitOutputFinal.Save("results_nunit.xml")
